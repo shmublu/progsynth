e99cd00db68d975d703a4a776a6761088ffb8fda
shmublu <samuelalexanderberman@gmail.com>
2021-03-24 22:09:18 -0400

    preliminary support for dual layer synthesis

--- queries/queryTemplate.sl
+++ queries/queryTemplate.sl
@@ -1,19 +1,11 @@
 (set-logic SLIA)
-(synth-fun concat ((s1 String)) String 
-((Start String (ntString))
-    (ntString String (s1 " " "," ")"(str.++ ntString ntString)
-      (str.replace ntString ntString ntString)
-      (str.at ntString ntInt)
-      (str.substr ntString ntInt ntInt)))
-    (ntInt Int ( 0 1 2 3 4 5
-      (+ ntInt ntInt)
-      (- ntInt ntInt)
-      (* ntInt ntInt)
-      (str.len ntString)
-      (str.indexof ntString ntString ntInt)))
-    (ntBool Bool ( true false(str.prefixof ntString ntString)
-      (str.suffixof ntString ntString)
-      (str.contains ntString ntString)))))
-(declare-var s1 String)
+
+(synth-fun f ((_arg_0 String)) String
+    ((Start String) (ntString String) (ntInt Int) (ntBool Bool))
+    ((Start String (ntString))
+    (ntString String (_arg_0 "" " " "<" ">" (str.++ ntString ntString) (str.replace ntString ntString ntString) (str.at ntString ntInt) (str.substr ntString ntInt ntInt)))
+    (ntInt Int (1 2 3 4 5 0 (- 1) (+ ntInt ntInt) (- ntInt ntInt) (str.len ntString) (str.indexof ntString ntString ntInt)))
+    (ntBool Bool (true false (= ntInt ntInt) (str.prefixof ntString ntString) (str.suffixof ntString ntString) (str.contains ntString ntString)))))
 #PART#
+
 (check-synth)
--- synth/query.js
+++ synth/query.js
@@ -3,9 +3,9 @@ var sygProc = require('./sygusProcessing.js');
 var smt_parser = require('./parsers/smt_parser.js');
 fs = require('fs');
 
-function sygusQuery(dirName="../queries") {
+function sygusQuery(inputs, outputs, dirName = "../queries") {
 //first, try and run it with all the constraints.
-	var query = buildQuery(dirName);
+	var query = buildQuery(inputs, outputs, dirName);
 	var queryOutput = runQuery(query);
 //if it doesn't error, great!
 	if(Boolean(queryOutput)) {
@@ -26,6 +26,7 @@ function sygusQuery(dirName="../queries") {
 		}
 		if( sygProc.areSolsIdentical(arrayOfSols)) {
 			var newConstraints= sygProc.getLoopRep(arrayOfSols);
+			console.log(newConstraints);
 			return newConstraints;
 		}
 		return null;
@@ -39,7 +40,7 @@ function runQuery(queryString="", queryFilePath='./queries/query.sl') {
 	fs.writeFileSync(queryFilePath, queryString, "utf8");
 	}
 	try{
-		cvc4Output = execSync('doalarm () { perl -e \'alarm shift; exec @ARGV\' "$@"; }\n doalarm 8 cvc4 '+queryFilePath).toString();
+		cvc4Output = execSync('doalarm () { perl -e \'alarm shift; exec @ARGV\' "$@"; }\n doalarm 1 cvc4 '+queryFilePath).toString();
 		return cvc4Output;
 	}
 	catch(error) {
@@ -48,8 +49,8 @@ function runQuery(queryString="", queryFilePath='./queries/query.sl') {
 	}
 }
 
-//Takes a directory name and looks for queryTemplate.sl and constraints.sl. If constraintNumber>-1, it builds a query using just that constraint
-function buildQuery(dirName="../queries", constraintNumber=-1) {
+/*Takes a directory name and looks for queryTemplate.sl and constraints.sl. If constraintNumber>-1, it builds a query using just that constraint
+function buildQueryOLD(dirName="../queries", constraintNumber=-1) {
 var body = fs.readFileSync(dirName + "/queryTemplate.sl","utf8");
 body = body.split("#PART#\n");
 var constraints = fs.readFileSync(dirName + "/constraints.sl","utf8"); 
@@ -66,15 +67,36 @@ else {
 	}
 }
 
+} */
+
+//Takes a directory name and looks for queryTemplate.sl and constraints.sl. If constraintNumber>-1, it builds a query using just that constraint
+function buildQuery(inputs, outputs, dirName="./queries", constraintNumber=-1) {
+var body = fs.readFileSync(dirName + "/queryTemplate.sl","utf8");
+body = body.split("#PART#\n");
+var constraints = buildConstraints(inputs,outputs);
+if(constraintNumber < 0) {
+	return body[0] + constraints + body[1];	
 }
+else {
+	constraints = constraints.split("\n");
+	if(constraints.length-1 > constraintNumber) {
+		return body[0] + constraints[constraintNumber] + body[1];
+	}
+	else {
+		return null;
+	}
+}
+
+}
+//Takes an array of inputs and outputs and puts them together in a string, separated by newlines
 function buildConstraints(arrayOfInputs, arrayOfOutputs, functionName = "f") {
-var b = "(constraint (= (" + functionName+ " \"";
-var m = "\") \"";
-var e = "\"))\n";
-var constraints = [];
+var b = "(constraint (= (" + functionName+ " ";
+var m = ") ";
+var e = "))\n";
+var constraints = "";
 for(var i = 0; (i < arrayOfInputs.length && i < arrayOfOutputs.length); i++) {
 	var constraint = b+ arrayOfInputs[i] +m + arrayOfOutputs[i] + e;
-	constraints.push(constraint);
+	constraints = constraints + constraint
 }
 return constraints;
 
